e367a2478e542e6827769776b472283e4d5d8639
Aellun <okello.k.otieono@gmail.com>
2023-09-22 21:37:41 -0700

    fixed main.c betty style requirements

--- main.c
+++ main.c
@@ -1,47 +1,52 @@
 #include "monty.h"
 #include <stdio.h>
-
-bus_t bus = {NULL, NULL, NULL, 0};
 #define MAX_LINE_LENGTH 1024
+/**
+ * main - code interpreter for monty
+ * @argc: number of arguments to passed at run time
+ * @argv: file location monty
+ * Return: 0 on success
+ */
 
 int main(int argc, char *argv[])
 {
-    char content[MAX_LINE_LENGTH];
-    FILE *file;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
-    size_t length;
+	char content[MAX_LINE_LENGTH];
+	FILE *file;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
+	size_t length;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    bus.file = file;
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	bus_t bus = {NULL, NULL, NULL, 0};
 
-    while (fgets(content, sizeof(content), file))
-    {
-        bus.content = content;
-        counter++;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-        length = strlen(content);
+	while (fgets(content, sizeof(content), file))
+	{
+		bus.content = content;
+		counter++;
 
-        if (length > 0 && content[length - 1] == '\n')
-        {
-            content[length - 1] = '\0';
-        }
+		length = strlen(content);
 
-        execute(content, &stack, counter, file);
-    }
+		if (length > 0 && content[length - 1] == '\n')
+		{
+			content[length - 1] = '\0';
+		}
 
-    free_stack(stack);
-    fclose(file);
-    return (0);
+		execute(content, &stack, counter, file);
+	}
+
+	free_stack(stack);
+	fclose(file);
+	return (0);
 }
-
